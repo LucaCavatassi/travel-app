eb605ae8b87a10316c35ce3c500bce31428081a8
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-08-30 09:43:55 +0200

    feat: footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,7 +5,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col">
-                <span>Footer</span>
+                <ul class="d-flex gap-2 align-items-center m-0 h-100 p-0">
+                    <li>© 2024 Travel-Diary</li>
+                    <li>-</li>
+                    <li>Developed by <a href="https://github.com/LucaCavatassi">Luca Maria Cavatassi</a></li>
+                </ul>
             </div>
         </div>
     </div>
@@ -19,6 +23,19 @@
         background-color: $blue;
         .col{
             color: white;
-        }
+            ul{
+                text-decoration: none;
+                list-style: none;
+                li {
+                    font-size: 0.8rem;
+                    a {
+                        color: $purple;
+                    }
+                    a:hover {
+                        color: $light-purple;
+                    }
+                }
+            }
+        }  
     }
 </style>
